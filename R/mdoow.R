4390e408325aa83b256d99ae93a8e33e878373d4
Thomas Levine <_@thomaslevine.com>
2015-08-19 09:31:16 -0400

    websocket

--- R/mdoow.R
+++ R/mdoow.R
@@ -1,5 +1,27 @@
-library(htmlwidgets)
+library(httpuv)
 
-htmlwidgets::createWidget("sigma", x, width = width, height = height)
+app <- list(
+  call = function(req) {
+  # wsUrl = paste(sep='',
+  #               '"',
+  #               "ws://",
+  #               ifelse(is.null(req$HTTP_HOST), req$SERVER_NAME, req$HTTP_HOST),
+  #               '"')
+    list(
+      status = 200L,
+      headers = list(
+        'Content-Type' = 'text/html'
+      ),
+      body = '<html>Hi</html>'
+    )
+  },
+  onWSOpen = function(ws) {
+    ws$onMessage(function(binary, message) {
+      ws$send(message)
+    })
+  }
+)
 
-encode <- function() 3
+host <- '0.0.0.0'
+port <- 9454
+runServer(host, port, app)
