9232e4ae4242901059d0bf6dc3ac166d90d20728
Thomas Levine <_@thomaslevine.com>
2015-08-19 09:38:18 -0400

    copy from demo

--- R/mdoow.R
+++ R/mdoow.R
@@ -2,17 +2,30 @@ library(httpuv)
 
 app <- list(
   call = function(req) {
-  # wsUrl = paste(sep='',
-  #               '"',
-  #               "ws://",
-  #               ifelse(is.null(req$HTTP_HOST), req$SERVER_NAME, req$HTTP_HOST),
-  #               '"')
+    wsUrl = paste0("ws://", ifelse(is.null(req$HTTP_HOST), req$SERVER_NAME, req$HTTP_HOST))
     list(
       status = 200L,
       headers = list(
         'Content-Type' = 'text/html'
       ),
-      body = '<html>Hi</html>'
+      body = sprintf('
+<html>
+<script>
+var ws = new WebSocket("%s")
+ws.onmessage = function(msg) {
+  var msgDiv = document.createElement("pre");
+  alert(msg.data)
+}
+function sendInput() {
+  var input = document.getElementById("input");
+  ws.send(input.value);
+  input.value = "";
+}
+</script>
+<h3>Send Message</h3>
+<form action="" onsubmit="sendInput(); return false">
+<input type="text" id="input"/>
+</html>', wsUrl)
     )
   },
   onWSOpen = function(ws) {
