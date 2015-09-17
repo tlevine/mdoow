8b879159b939b9e72112675d54188f0c6f5806ab
Thomas Levine <_@thomaslevine.com>
2015-09-17 09:30:03 -0400

    j

--- examples/serve.R
+++ examples/serve.R
@@ -2,4 +2,9 @@ source('R/mdoow.R')
 library(httpuv)
 
 app <- list(call = http.call)
-startServer('0.0.0.0', 8888, app)
+handle <- startServer('0.0.0.0', 8888, app)
+
+replicate(10, service())
+mdoow(iris[-5])
+
+stopServer(handle)
