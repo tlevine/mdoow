78d513902b2876e8b9838008a16f49e339487abe
Thomas Levine <_@thomaslevine.com>
2016-02-17 10:21:19 +0000

    dependencies

--- default.nix
+++ default.nix
@@ -2,9 +2,9 @@ with import <nixpkgs> {}; {
   mdoowEnv = stdenv.mkDerivation {
     name = "mdoow";
     buildInputs = [
-      rPackages.htmlwidgets rPackages.htmltools
+      rPackages.htmlwidgets
       rPackages.devtools rPackages.httpuv rPackages.tuneR
     ];
-    R_LIBS = "${rPackages.devtools}/library:${rPackages.httpuv}/library:${rPackages.tuneR}/library:${rPackages.htmlwidgets}/library:${rPackages.htmltools}/library";
+    R_LIBS = "${rPackages.devtools}/library:${rPackages.httpuv}/library:${rPackages.tuneR}/library:${rPackages.htmlwidgets}/library:${rPackages.htmltools}/library:${rPackages.shiny}/library:${rPackages.xtable}/library";
   };  
 }
