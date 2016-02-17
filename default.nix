310d83ec3e398455ddc7d250784fec06380fd637
Thomas Levine <_@thomaslevine.com>
2016-02-17 09:58:25 +0000

    htmlwidgets dependency

--- default.nix
+++ default.nix
@@ -2,8 +2,9 @@ with import <nixpkgs> {}; {
   mdoowEnv = stdenv.mkDerivation {
     name = "mdoow";
     buildInputs = [
+      rPackages.htmlwidgets rPackages.htmltools
       rPackages.devtools rPackages.httpuv rPackages.tuneR
     ];
-    R_LIBS = "${rPackages.devtools}/library:${rPackages.httpuv}/library:${rPackages.tuneR}/library";
+    R_LIBS = "${rPackages.devtools}/library:${rPackages.httpuv}/library:${rPackages.tuneR}/library:${rPackages.htmlwidgets}/library:${rPackages.htmltools}/library";
   };  
 }
