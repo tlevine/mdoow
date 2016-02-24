f6eadfaffc760fb8c4f348e435f09f8b0e645dbb
Thomas Levine <_@thomaslevine.com>
2016-02-24 22:41:38 +0000

    -

--- play.r
+++ play.r
@@ -1,10 +1,18 @@
-t <- 1:8000
-A4 <- sin(440 * t * 2 * pi / 8000)
+t <- 1:4000
 
-U8 <- function(x) floor((x+1)*127.5)
-noise <- floor(runif(length(t), 0, 255))
-silence <- rep(127, 8000)
+A4 <- sin(440 * t * 2 * pi / 8000)
+noise <- floor(runif(length(t)), -1, 1)
+silence <- rep(0, 4000)
+
+# scale.wave <- function(x)
+#   -1 + 2 * ((x - min(x)) / (max(x) - min(x)))
+
+U8 <- function(x)
+  if (any(is.na(x)) || min(x) < -1 || max(x) > 1)
+    stop('You must normalize and remove NAs.')
+  else
+    as.raw(floor(x*127.5))
 
 dsp <- file('/dev/dsp', open = 'wb')
-writeBin(as.raw(U8(A4)), dsp)
+writeBin(U8(A4), dsp)
 close(dsp)
