9585539f54b28e149b8218f84cb8c5fd38ae7a2d
Thomas Levine <_@thomaslevine.com>
2016-02-24 20:38:02 +0000

    woo single-threaded dsp writing

--- loop.r
+++ loop.r
@@ -2,19 +2,15 @@ BITRATE = 8000 # per second
 BUFFER = 1/12 # seconds
 options(digits.secs = 9)
 
-service.loop <- function(next.sample,
-                         prev.time = NULL, prev.sample = NULL) {
-  if (is.null(prev.time) || is.null(prev.sample))
+#' @export
+loop <- function(next.sample, prev.time = NULL) {
+  if (is.null(prev.time))
     next.time <- Sys.time()
   else
-    next.time <- prev.time + next.sample / BITRATE
+    next.time <- prev.time + length(next.sample) / BITRATE
 
   if (next.time - (BITRATE * BUFFER) < Sys.time()) {
-    # XXX Write to /dev/dsp
-    function(x) service.loop(x, prev.time = next.time,
-                             next.sample = prev.sample)
+    cat('XXX Write to /dev/dsp\n')
+    function(x) loop(x, prev.time = next.time)
   }
 }
-
-#' @export
-loop <- function(first.sample) service.loop(first.sample)
