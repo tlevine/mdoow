5721d00709fc319808b97301954cda185042528f
Thomas Levine <_@thomaslevine.com>
2016-02-24 22:43:05 +0000

    Automatic commit with j

--- play.r
+++ play.r
@@ -1,17 +1,18 @@
 t <- 1:4000
 
 A4 <- sin(440 * t * 2 * pi / 8000)
-noise <- floor(runif(length(t)), -1, 1)
+noise <- floor(runif(length(t), -1, 1))
 silence <- rep(0, 4000)
 
 # scale.wave <- function(x)
 #   -1 + 2 * ((x - min(x)) / (max(x) - min(x)))
 
-U8 <- function(x)
+U8 <- function(x) {
   if (any(is.na(x)) || min(x) < -1 || max(x) > 1)
     stop('You must normalize and remove NAs.')
   else
-    as.raw(floor(x*127.5))
+    as.raw(128 + floor(x*127.5))
+}
 
 dsp <- file('/dev/dsp', open = 'wb')
 writeBin(U8(A4), dsp)
